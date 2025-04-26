5486db7f740787298aa0c5da4f16252818052ee8
LohYenKuan <lohyenkuan@gmail.com>
2025-04-26 14:33:14 +0800

    Dashboard Layout

---
+++ four-alarm-fire/src/components/app-sidebar.tsx
@@ -0,0 +1,54 @@
+import { NotebookText, Home } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+// Menu items.
+const items = [
+  {
+    title: "Dashboard",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Statement",
+    url: "#",
+    icon: NotebookText,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent className="bg-black text-white">
+        <SidebarGroup>
+          <SidebarGroupLabel className="text-gray-300 text-2xl font-semibold ml-4 mb-4 mt-4">
+            intelly
+          </SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
